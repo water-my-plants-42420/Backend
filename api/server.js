776bf8eb5e09015bccbc5cd89579e01302b24f57
Michael Ross <mross.j@gmail.com>
2020-04-27 16:55:16 -0400

    update server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require("cors");
+const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require("../users/authenticate-middleware");
+const authenticate = require('../users/authenticate-middleware');
 usersRouter = require('../users/users-router');
-authRouter = require("../auth/auth-router");
+authRouter = require('../auth/auth-router');
 
 const server = express();
 
@@ -12,7 +12,11 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter)
+server.get('/', (req, res) => {
+	res.status(200).json({ message: `api up` });
+});
+
+server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 module.exports = server;
