512aadfa5735e6d8a54b22a79ef619d1cbd1a67e
Michael Ross <mross.j@gmail.com>
2020-04-28 18:12:50 -0400

    update auth-router
    * updated auth login
    * updated table with first and last name

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,7 +23,7 @@ router.post('/register', (req, res) => {
 		});
 });
 
-router.post('.login', (req, res) => {
+router.post('/login', (req, res) => {
 	let { username, password } = req.body;
 	Users.findBy({
 		username,
--- migrations/20200427102731_users.js
+++ migrations/20200427102731_users.js
@@ -1,22 +1,28 @@
 exports.up = function (knex) {
-    return knex.schema
-        .createTable('users', (users) => {
-            users.increments()
-            users.string('username', 128).notNullable().unique()
-            users.string('password', 128).notNullable()
-            users.integer('phone', 15).notNullable()
-        })
+	return knex.schema
+		.createTable('users', (users) => {
+			users.increments();
+			profiles.string('first_name');
+			profiles.string('last_name');
+			users.string('username', 128).notNullable().unique();
+			users.string('password', 128).notNullable();
+			users.integer('phone', 15).notNullable();
+		})
 
-        .createTable('plants', plants => {
-            plants.increments()
-            plants.string('name', 128).notNullable()
-            plants.string('species', 128).notNullable()
-            plants.string('water_freq', 128).notNullable()
-            plants.integer('user_id').notNullable().unsigned().references('id').inTable('users');
-        })
+		.createTable('plants', (plants) => {
+			plants.increments();
+			plants.string('name', 128).notNullable();
+			plants.string('species', 128).notNullable();
+			plants.string('water_freq', 128).notNullable();
+			plants
+				.integer('user_id')
+				.notNullable()
+				.unsigned()
+				.references('id')
+				.inTable('users');
+		});
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('users')
-        .dropTableIfExists('plants');
+	return knex.schema.dropTableIfExists('users').dropTableIfExists('plants');
 };
