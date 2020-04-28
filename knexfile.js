a653f2d6e2657dadaffe16afda6c7b096989e2da
Michael Ross <mross.j@gmail.com>
2020-04-28 18:07:45 -0400

    add database
    * updated knexfile and added db3

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,24 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+	development: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: './data/auth.db3',
+		},
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done);
+			},
+		},
+		migrations: {
+			directory: './migrations',
+		},
+		seeds: {
+			directory: './seeds',
+		},
+	},
 
   staging: {
     client: 'postgresql',
