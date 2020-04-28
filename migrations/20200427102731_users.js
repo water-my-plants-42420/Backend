68aa3647aee644edc74913cf1d70d351765d865d
Michael Ross <mross.j@gmail.com>
2020-04-28 19:08:02 -0400

    update table

--- migrations/20200427102731_users.js
+++ migrations/20200427102731_users.js
@@ -2,8 +2,8 @@ exports.up = function (knex) {
 	return knex.schema
 		.createTable('users', (users) => {
 			users.increments();
-			profiles.string('first_name', 128);
-			profiles.string('last_name', 128);
+			users.string('first_name', 128);
+			users.string('last_name', 128);
 			users.string('username', 128).notNullable().unique();
 			users.string('password', 128).notNullable();
 			users.integer('phone', 15).notNullable();
